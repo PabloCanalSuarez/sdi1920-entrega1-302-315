0ecfdda0abd467654790a31fac96fabcae02e1f0
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-03-05 20:37:35 +0100

    Primer commit - Creación del proyecto

---
+++ sdi1920-entrega1-302315/src/test/java/com/uniovi/Sdi1920Entrega1302315ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi1920Entrega1302315ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
